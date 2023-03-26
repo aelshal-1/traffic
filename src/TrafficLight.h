5626ff0954a9b73a0482b730b4dc680e5d48ce5e
Ahmed El-Shal <ahmed_elshal@seegypt.com>
2023-03-26 19:04:57 +0200

    FP.3

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -8,7 +8,10 @@
 
 // forward declarations to avoid include cycle
 class Vehicle;
-
+enum TrafficLightPhase{
+    red,
+    green
+};
 
 // FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
 // Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type. 
@@ -19,9 +22,11 @@ template <class T>
 class MessageQueue
 {
 public:
-
+    T receive();
+    void send(T&& phase);
 private:
-    
+    std::deque<T> _queue;
+    std::condition_variable _cond;
 };
 
 // FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
@@ -30,10 +35,7 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
-enum TrafficLightPhase{
-    red,
-    green
-};
+
 class TrafficLight : public TrafficObject
 {
 public:
