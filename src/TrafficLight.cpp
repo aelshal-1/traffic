192fdc9c5af5215c378e7e48f1b53cb02ded1b76
Ahmed El-Shal <ahmed_elshal@seegypt.com>
2023-03-26 18:59:37 +0200

    partial FP.2

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -44,6 +44,7 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 void TrafficLight::simulate()
 {
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
@@ -53,6 +54,27 @@ void TrafficLight::cycleThroughPhases()
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
-}
 
-*/
+    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dis(4.0, 6.0);
+    double cycleDuration = dis(gen);
+    std::chrono::time_point<std::chrono::system_clock> lastUpdate;
+	lastUpdate = std::chrono::system_clock::now();
+    while (true){
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        long timeSinceLastUpdate = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - lastUpdate).count();
+        if (timeSinceLastUpdate >= cycleDuration){
+            if(_currentPhase == TrafficLightPhase::red){
+                _currentPhase = TrafficLightPhase::green;
+            }else{
+                _currentPhase = TrafficLightPhase::red;
+            }
+
+            //send update
+        }
+        lastUpdate = std::chrono::system_clock::now();
+    }
+
+}
